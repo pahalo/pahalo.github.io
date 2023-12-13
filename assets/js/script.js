da1922199fe3093518c5d23f48dd0bb41a385c34
Unf4ir <paulclasht@gmail.com>
2023-12-13 21:33:39 +0100

    Change of the donation part(red color andnewarrow)

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,18 @@ window.addEventListener('scroll', function() {
         document.querySelector('.logo').classList.add('hidden');
     }
 });
-// JavaScript, um den Text beim Klicken auf den Toggle-Button anzuzeigen bzw. zu verbergen
-document.addEventListener("DOMContentLoaded", function() {
-    var toggleButton = document.getElementById('toggle');
-    var spendenText = document.querySelector('.spenden-text');
-
-    toggleButton.addEventListener('click', function(e) {
-        e.preventDefault();
-        spendenText.classList.toggle('active');
-        toggleButton.classList.toggle('active');
-        spendenText.style.display = spendenText.classList.contains('active') ? 'block' : 'none';
-    });
-});
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.getElementById('toggle');
     const spendenText = document.getElementById('spendenText');
 
-    toggleButton.addEventListener('click', function() {
-        if (spendenText.style.display === 'none' || spendenText.style.display === '') {
-            spendenText.style.display = 'block';
-            toggleButton.classList.add('active');
-        } else {
-            spendenText.style.display = 'none';
+    // Beim Klick auf den Toggle-Button
+    toggleButton.addEventListener('click', function () {
+        if (spendenText.classList.contains('active')) {
+            spendenText.classList.remove('active');
             toggleButton.classList.remove('active');
+        } else {
+            spendenText.classList.add('active');
+            toggleButton.classList.add('active');
         }
     });
 });
